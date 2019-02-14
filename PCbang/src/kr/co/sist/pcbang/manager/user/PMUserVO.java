518d85a92ef0945dad9238ce69ecc40816b3ca80
sist <sist@DESKTOP-SLKR09J>
2019-02-14 15:58:37 +0900

    user class update2

--- PCbang/src/kr/co/sist/pcbang/manager/user/PMUserVO.java
+++ PCbang/src/kr/co/sist/pcbang/manager/user/PMUserVO.java
@@ -1,5 +1,69 @@
 package kr.co.sist.pcbang.manager.user;
 
 public class PMUserVO {
+	
+	private int leftTime, totalPrice;
+	  private String id, name, birth, gender, tel, email, detailAddress, inputDate;
 
+	  public PMUserVO(String id, String name, String birth, String gender, String tel, String email,
+			String detailAddress, int leftTime, int totalPrice, String inputDate) {
+		this.id = id;
+		this.name = name;
+		this.birth = birth;
+		this.gender = gender;
+		this.tel = tel;
+		this.email = email;
+		this.detailAddress = detailAddress;
+		this.leftTime = leftTime;
+		this.totalPrice = totalPrice;
+		this.inputDate = inputDate;
+	}
+
+	  
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public String getDetailAddress() {
+		return detailAddress;
+	}
+
+	public int getLeftTime() {
+		return leftTime;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public String getInputDate() {
+		return inputDate;
+	}
+
+	
+	
+	
+	
+	
 }
