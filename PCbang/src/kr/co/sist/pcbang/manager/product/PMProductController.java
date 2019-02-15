835055d4acbe980d2ee769d8f73144b6cc4d0626
minj0i <h_hoh@naver.com>
2019-02-15 17:56:52 +0900

    진짜진짜2월15일

--- PCbang/src/kr/co/sist/pcbang/manager/product/PMProductController.java
+++ PCbang/src/kr/co/sist/pcbang/manager/product/PMProductController.java
@@ -4,7 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -15,10 +17,20 @@ import kr.co.sist.pcbang.manager.product.add.PMProductAddView;
 
 public class PMProductController extends MouseAdapter implements ActionListener {
 
+	public static List<String> PrdImgList;
 	private PMProductView pmpv;
 	private PMProductDAO pmpdao;
 
 	public PMProductController(PMProductView pmpv) {
+		PrdImgList=new ArrayList<String>();
+		//서버에 존재하는 이미지 입력
+		
+		//이미지를 근데 FileServer로 돌려야 하는데 어디서 돌리지????
+		File file = new File("C:/Users/owner/git/pcbang/PCbang/src/kr/co/sist/pcbang/manager/product/img");
+		for(String tempName:file.list()) {
+			PrdImgList.add(tempName);
+		}//end for
+		
 		this.pmpv = pmpv;
 		pmpdao = PMProductDAO.getInstance();
 		// 상품 목록을 초기화한다.
@@ -35,7 +47,7 @@ public class PMProductController extends MouseAdapter implements ActionListener 
 		try {
 			// DB에서 상품 정보를 조회
 			List<PMProductVO> listproduct = pmpdao.selectPrd();
-			
+
 			// JTable에 조회한 정보를 출력
 			PMProductVO pmpvo = null;
 			String imgPath = "C:/Users/owner/git/pcbang/PCbang/src/kr/co/sist/pcbang/manager/product/img";
@@ -59,16 +71,16 @@ public class PMProductController extends MouseAdapter implements ActionListener 
 
 			if (listproduct.isEmpty()) {// 입력된 상품이 없을 때
 
-					rowData = new Object[6];
-					rowData[0] = "";
-					rowData[1] = "입력된";
-					rowData[2] = "상품이";
-					rowData[3] = "없습니다";
-					rowData[4] = "";
-					rowData[5] = "";
+				rowData = new Object[6];
+				rowData[0] = "";
+				rowData[1] = "입력된";
+				rowData[2] = "상품이";
+				rowData[3] = "없습니다";
+				rowData[4] = "";
+				rowData[5] = "";
 
-					// DTM에 추가
-					dtmPrd.addRow(rowData);
+				// DTM에 추가
+				dtmPrd.addRow(rowData);
 			} // end if
 
 		} catch (SQLException e) {
@@ -81,24 +93,24 @@ public class PMProductController extends MouseAdapter implements ActionListener 
 	 * 카테고리와 상품명으로 찾기
 	 */
 	public void searchPrd() {
-		DefaultTableModel dtmSearch=pmpv.getDtmPrd();
+		DefaultTableModel dtmSearch = pmpv.getDtmPrd();
 		dtmSearch.setRowCount(0);
-		
-		String category=pmpv.getJcbPrdCategory().getSelectedItem().toString();
-		String menuName=pmpv.getJtfPrdName().getText();
+
+		String category = pmpv.getJcbPrdCategory().getSelectedItem().toString();
+		String menuName = pmpv.getJtfPrdName().getText();
 		try {
-			List<PMSchProductVO> list_pmspvo=pmpdao.searchPrd(category.toString(), menuName);
-			Object[] rowData=null;
-			PMSchProductVO pmspvo=null;
-			for(int i=0; i<list_pmspvo.size(); i++) {
-				pmspvo=list_pmspvo.get(i);
-				rowData=new Object[6];
-				rowData[0]=(pmspvo.getMenuCode());
-				rowData[1]=(pmspvo.getMenuName());
-				rowData[2]=(pmspvo.getImg());
-				rowData[3]=(pmspvo.getPrice());
-				rowData[4]=(pmspvo.getQuan());
-				rowData[5]=(pmspvo.getPrice()*pmspvo.getQuan());
+			List<PMSchProductVO> list_pmspvo = pmpdao.searchPrd(category.toString(), menuName);
+			Object[] rowData = null;
+			PMSchProductVO pmspvo = null;
+			for (int i = 0; i < list_pmspvo.size(); i++) {
+				pmspvo = list_pmspvo.get(i);
+				rowData = new Object[6];
+				rowData[0] = (pmspvo.getMenuCode());
+				rowData[1] = (pmspvo.getMenuName());
+				rowData[2] = (pmspvo.getImg());
+				rowData[3] = (pmspvo.getPrice());
+				rowData[4] = (pmspvo.getQuan());
+				rowData[5] = (pmspvo.getPrice() * pmspvo.getQuan());
 
 				// DTM에 추가
 				dtmSearch.addRow(rowData);
@@ -106,12 +118,12 @@ public class PMProductController extends MouseAdapter implements ActionListener 
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 		} // end catch
-	}//searchPrd
-	
-	
-	///////////////초기화 내용추가 필요
-	public void reset() {}
-	
+	}// searchPrd
+
+	/////////////// 초기화 내용추가 필요
+	public void reset() {
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if (ae.getSource() == pmpv.getJbtAddPrd()) { // 도시락 추가 버튼
@@ -144,11 +156,18 @@ public class PMProductController extends MouseAdapter implements ActionListener 
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {	}
+	public void mousePressed(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseReleased(MouseEvent e) {	}
+	public void mouseReleased(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseEntered(MouseEvent e) {	}
+	public void mouseEntered(MouseEvent e) {
+	}
+
 	@Override
-	public void mouseExited(MouseEvent e) {	}
+	public void mouseExited(MouseEvent e) {
+	}
 }// class
--- PCbang/src/kr/co/sist/pcbang/manager/product/add/PMProductAddController.java
+++ PCbang/src/kr/co/sist/pcbang/manager/product/add/PMProductAddController.java
@@ -27,60 +27,60 @@ public class PMProductAddController extends WindowAdapter implements ActionListe
 		uploadImg = "";
 	}//생성자
 	
-//	/**
-//	 * 입력값과 이미지를 선택한 후 유효성 검증(이미지, 상품명, 카테고리, 가격이 입력되었다면)
-//	 */
-//	private void addPrd() {
-//		if(uploadImg.equals("")) {
-//			JOptionPane.showMessageDialog(pmpav, "이미지를 선택해주세요");
-//			return;
-//		}//end if
-//		
-//		JTextField jtfName = pmpav.getJtfMenuName();
-//		JComboBox<String> jcbCategory = pmpav.getJcbPrdCategory();
-//		JTextField jtfPrice = pmpav.getJtfPrice();
-//		
-//		if(jtfName.getText().trim().equals("")) {
-//			JOptionPane.showMessageDialog(pmpav, "상품명은 필수 입력!!!!");
-//			jtfName.setText("");
-//			jtfName.requestFocus();
-//			return;
-//		}//end if
-//		
-//		if(jtfPrice.getText().trim().equals("")) {
-//			JOptionPane.showMessageDialog(pmpav, "상품 가격은 필수 입력!!!!");
-//			jtfPrice.setText("");
-//			jtfPrice.requestFocus();
-//			return;
-//		}//end if
-//		
-//		//가격은 숫자만 입력되도록 try~catch
-//		int price = 0;
-//		try {
-//			price = Integer.parseInt(jtfPrice.getText().trim());
-//		}catch(NumberFormatException nfe) {
-//			JOptionPane.showMessageDialog(pmpav, "가격은 숫자만 입력가능합니다.");
-//			return;
-//		}//end catch
-//		
-//		File file = new File(uploadImg);
-//		
-//		PMProductAddVO pmpavo= new PMProductAddVO(jtfName.getText().trim(), 
-//				file.getName(), jcbCategory.getSelectedItem().toString(), price);
-//		
-//		try {
-//			PMProductDAO.getInstance().insertPrd(pmpavo); //DB에 추가
-//			//이미지를, 사용하는 폴더로 복사 (=스트림 필요)
-//			uploadImg(file);
-//			//파일리스트에 새로운 파일명을 추가한다.
-//			LunchAdminMain.lunchImageList.add(file.getName());
-//			
-//			//LunchDetailController에  modify 큰이미지, 작은이미지에 관한 내용을 넣으면서 다시 강사님 코드로 고침
-////			uploadImg(file);
-////			//에러가 나면 catch로 빠져서 DB에 추가가 안됨
-////			LunchAdminDAO.getInstance().insertLunch(lavo);//에러가 나지 않는 경우 DB에 추가
-//			
-//			//리스트 갱신
+	/**
+	 * 입력값과 이미지를 선택한 후 유효성 검증(이미지, 상품명, 카테고리, 가격이 입력되었다면)
+	 */
+	private void addPrd() {
+		if(uploadImg.equals("")) {
+			JOptionPane.showMessageDialog(pmpav, "이미지를 선택해주세요");
+			return;
+		}//end if
+		
+		JTextField jtfName = pmpav.getJtfMenuName();
+		JComboBox<String> jcbCategory = pmpav.getJcbPrdCategory();
+		JTextField jtfPrice = pmpav.getJtfPrice();
+		
+		if(jtfName.getText().trim().equals("")) {
+			JOptionPane.showMessageDialog(pmpav, "상품명은 필수 입력!!!!");
+			jtfName.setText("");
+			jtfName.requestFocus();
+			return;
+		}//end if
+		
+		if(jtfPrice.getText().trim().equals("")) {
+			JOptionPane.showMessageDialog(pmpav, "상품 가격은 필수 입력!!!!");
+			jtfPrice.setText("");
+			jtfPrice.requestFocus();
+			return;
+		}//end if
+		
+		//가격은 숫자만 입력되도록 try~catch
+		int price = 0;
+		try {
+			price = Integer.parseInt(jtfPrice.getText().trim());
+		}catch(NumberFormatException nfe) {
+			JOptionPane.showMessageDialog(pmpav, "가격은 숫자만 입력가능합니다.");
+			return;
+		}//end catch
+		
+		File file = new File(uploadImg);
+		
+		PMProductAddVO pmpavo= new PMProductAddVO(jtfName.getText().trim(), 
+				file.getName(), jcbCategory.getSelectedItem().toString(), price);
+		
+		try {
+			PMProductDAO.getInstance().insertPrd(pmpavo); //DB에 추가
+			//이미지를, 사용하는 폴더로 복사 (=스트림 필요)
+			uploadImg(file);
+			//파일리스트에 새로운 파일명을 추가한다.
+			PMProductController.PrdImgList.add(file.getName());
+			
+			//LunchDetailController에  modify 큰이미지, 작은이미지에 관한 내용을 넣으면서 다시 강사님 코드로 고침
+			uploadImg(file);
+//			//에러가 나면 catch로 빠져서 DB에 추가가 안됨
+//			LunchAdminDAO.getInstance().insertLunch(lavo);//에러가 나지 않는 경우 DB에 추가
+			
+			//리스트 갱신
 //			lmc.setLunch();
 //			
 //			//다음 도시락의 입력을 편하게 하기 위해서 입력폼 초기화
@@ -91,14 +91,22 @@ public class PMProductAddController extends WindowAdapter implements ActionListe
 //			lav.getJlLunchImg().setIcon(new ImageIcon("C:/dev/workspace/lunch_prj/src/kr/co/sist/lunch/admin/img/no_img.jpg"));
 //			
 //			JOptionPane.showMessageDialog(lav, "도시락이 추가되었습니다.");
-//		} catch (IOException ie) {
-//			JOptionPane.showMessageDialog(lav, "파일 업로드 실패");
-//			ie.printStackTrace();
-//		} catch (SQLException se) {
-//			JOptionPane.showMessageDialog(lav, "DB에서 문제가 발생하였습니다.");
-//			se.printStackTrace();
-//		}//end catch 
-//	}//addPrd
+		} catch (IOException ie) {
+			JOptionPane.showMessageDialog(pmpav, "파일 업로드 실패");
+			ie.printStackTrace();
+		} catch (SQLException se) {
+			JOptionPane.showMessageDialog(pmpav, "DB에서 문제가 발생하였습니다.");
+			se.printStackTrace();
+		}//end catch 
+	}//addPrd
+	
+	private void uploadImg(File file) throws IOException{
+		
+	}//uploadImg
+	
+	private void setImg() {
+		
+	}//setImg
 	
 	@Override
 	public void windowClosing(WindowEvent e) {
@@ -108,10 +116,10 @@ public class PMProductAddController extends WindowAdapter implements ActionListe
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		if(ae.getSource()==pmpav.getjbImg()) {
-//			setImg();
+			setImg();
 		}//end if
 		if(ae.getSource()==pmpav.getJbAdd()) {
-//			addProduct();
+			addPrd();
 		}//end if
 		
 		if(ae.getSource()==pmpav.getJbEnd()) {
