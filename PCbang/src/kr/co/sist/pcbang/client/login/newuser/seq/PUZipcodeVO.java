551f3da47e9aa5cd95d799a84063ce12cbe04ddd
JWKim <JWKim@119.192.63.199>
2019-02-20 22:50:14 +0900

    주소검색 update

--- PCbang/src/kr/co/sist/pcbang/client/login/newuser/seq/PUZipcodeVO.java
+++ PCbang/src/kr/co/sist/pcbang/client/login/newuser/seq/PUZipcodeVO.java
@@ -1,5 +1,39 @@
 package kr.co.sist.pcbang.client.login.newuser.seq;
 
 public class PUZipcodeVO {
-
-}
+	private String zipcode, sido, gugun, dong, bunji;
+	private int seq;
+	public PUZipcodeVO(String zipcode, String sido, String gugun, String dong, String bunji, int seq) {
+		this.zipcode = zipcode;
+		this.sido = sido;
+		this.gugun = gugun;
+		this.dong = dong;
+		this.bunji = bunji;
+		this.seq = seq;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public String getSido() {
+		return sido;
+	}
+	public String getGugun() {
+		return gugun;
+	}
+	public String getDong() {
+		return dong;
+	}
+	public String getBunji() {
+		return bunji;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	@Override
+	public String toString() {
+		return "PUZipcodeVO [zipcode=" + zipcode + ", sido=" + sido + ", gugun=" + gugun + ", dong=" + dong + ", bunji="
+				+ bunji + ", seq=" + seq + "]";
+	}
+	
+	
+}// class
