ee260bc18b4d9881343c6d7a46c131fbdf5cf3ff
JeongTeacksung <45474415+JeongTeacksung@users.noreply.github.com>
2019-02-20 16:17:46 +0900

    두번째 ㅊ촤촤촤촤지

--- PCbang/src/kr/co/sist/pcbang/client/charge/Test.java
+++ PCbang/src/kr/co/sist/pcbang/client/charge/Test.java
@@ -3,7 +3,7 @@ package kr.co.sist.pcbang.client.charge;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("나는야 정엉택성");
 
 	}
 
