75ea14e113f4be06cd2b3e493c511ec8f7371b57
owner <owner@DESKTOP-RO725E7>
2019-03-12 11:48:30 +0900

    19-03-12 통계 표 완성

--- PCbang/src/kr/co/sist/pcbang/manager/statics/PMStaticsView.java
+++ PCbang/src/kr/co/sist/pcbang/manager/statics/PMStaticsView.java
@@ -1,5 +1,6 @@
 package kr.co.sist.pcbang.manager.statics;
 
+import java.awt.Color;
 import java.util.Calendar;
 
 import javax.swing.ButtonGroup;
@@ -9,35 +10,42 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class PMStaticsView extends JPanel {
 	// 190304 이재찬 작성
-	private JComboBox<String> jcbStaticsChoice;
+	// private JComboBox<String> jcbStaticsChoice;
 	private JRadioButton jrbOperRate, jrbFoodSell, jrbTotalSell;
 	private JButton jbtFoodDetail, jbtCreateTable, jbtCreateGraph;
-	private JTextField jlbResult;
+	// private JTextField jlbResult;
 
 	private JComboBox<String> jcbBeforeYear, jcbBeforeMonth, jcbBeforeDay;
 	private JComboBox<String> jcbAfterYear, jcbAfterMonth, jcbAfterDay;
 	private JLabel jlb2;
-	
+
+	private DefaultTableModel dtmMonthOper, dtmDayOper, dtmMonthFood, dtmDayFood, dtmMonthTotal, dtmDayTotal, dtmMaxData, dtmSumData;
+	private JScrollPane jspMT, jspDT, jspMF, jspDF, jspMO, jspDO, jspMax, jspSum;
+
 	public PMStaticsView() {
-		String[] category = {"일간 통계", "주간 통계", "월간 통계"};
+		String[] category = { "일간 통계", "주간 통계", "월간 통계" };
 		DefaultComboBoxModel<String> dcbm = new DefaultComboBoxModel<String>(category);
-		jcbStaticsChoice = new JComboBox<String>(dcbm);
-		
+		// jcbStaticsChoice = new JComboBox<String>(dcbm);
+
 		jcbBeforeYear = new JComboBox<String>();
 		jcbBeforeMonth = new JComboBox<String>();
 		jcbBeforeDay = new JComboBox<String>();
-		
+
 		jcbAfterYear = new JComboBox<String>();
 		jcbAfterMonth = new JComboBox<String>();
 		jcbAfterDay = new JComboBox<String>();
-		
-		setDate(jcbBeforeYear, jcbBeforeMonth, jcbBeforeDay); // 
-		setDate(jcbAfterYear, jcbAfterMonth, jcbAfterDay); // 
-		
+
+		setDate(jcbBeforeYear, jcbBeforeMonth, jcbBeforeDay); //
+		setDate(jcbAfterYear, jcbAfterMonth, jcbAfterDay); //
+
 		ButtonGroup bg = new ButtonGroup();
 		jrbOperRate = new JRadioButton("가동률");
 		jrbFoodSell = new JRadioButton("음식 매출");
@@ -49,14 +57,132 @@ public class PMStaticsView extends JPanel {
 		jbtFoodDetail = new JButton("음식 상세 통계");
 		jbtCreateTable = new JButton("표 생성");
 		jbtCreateGraph = new JButton("그래프 생성");
-		jlbResult = new JTextField();
+
+		JPanel jlbResult;
+		jlbResult = new JPanel();
+		jlbResult.setBackground(Color.WHITE);
+		jlbResult.setLayout(null);
+
+		String[] monthOperColumns = { "주차", "가동율" };
+		String[] dayOperColumns = { "일자", "가동율" };
+		String[] maxColumns = { "일자", "최고값" };
+		String[] sumColumn = { "매출 총 합" };
+		dtmMonthOper = new DefaultTableModel(monthOperColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		dtmDayOper = new DefaultTableModel(dayOperColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		dtmMaxData = new DefaultTableModel(maxColumns, 1) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		dtmSumData = new DefaultTableModel(sumColumn, 1) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		JTable jtMonthOper = new JTable(dtmMonthOper);
+		JTable jtDayOper = new JTable(dtmDayOper);
+		JTable jtMax = new JTable(dtmMaxData);
+		JTable jtSum = new JTable(dtmSumData);
+
+		// 가동률 테이블 크기 설정 : 전체 200
+		jtMonthOper.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtMonthOper.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtMonthOper.setRowHeight(25);
+		jtDayOper.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtDayOper.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtDayOper.setRowHeight(25);
+		jtMax.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtMax.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtMax.setRowHeight(25);
+		jtSum.getColumnModel().getColumn(0).setPreferredWidth(340);
+		jtSum.setRowHeight(25);
+
+		jspMO = new JScrollPane(jtMonthOper);
+		jspDO = new JScrollPane(jtDayOper);
+		jspMax = new JScrollPane(jtMax);
+		jspSum = new JScrollPane(jtSum);
+		jspMO.setBorder(new TitledBorder("가동률 통계"));
+		jspDO.setBorder(new TitledBorder("가동률 통계"));
+
+		String[] monthFoodColumns = { "주차", "상품 매출액" };
+		String[] dayFoodColumns = { "일자", "상품 매출액" };
+		dtmMonthFood = new DefaultTableModel(monthFoodColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		dtmDayFood = new DefaultTableModel(dayFoodColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
 		
+		JTable jtMonthFood = new JTable(dtmMonthFood);
+		JTable jtDayFood = new JTable(dtmDayFood);
+
+		// 상품 매출액 테이블 크기 설정 : 전체 200
+		jtMonthFood.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtMonthFood.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtMonthFood.setRowHeight(25);
+		jtDayFood.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtDayFood.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtDayFood.setRowHeight(25);
+
+		jspMF = new JScrollPane(jtMonthFood);
+		jspDF = new JScrollPane(jtDayFood);
+		jspMF.setBorder(new TitledBorder("상품 매출 통계"));
+		jspDF.setBorder(new TitledBorder("상품 매출 통계"));
+
+		String[] monthTotalColumns = { "주차", "총 매출액" };
+		String[] dayTotalColumns = { "일자", "총 매출액" };
+		dtmMonthTotal = new DefaultTableModel(monthTotalColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		dtmDayTotal = new DefaultTableModel(dayTotalColumns, 4) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}// isCellEditable
+		};
+		JTable jtMonthTotal = new JTable(dtmMonthTotal);
+		JTable jtDayTotal = new JTable(dtmDayTotal);
+
+		// 상품 매출액 테이블 크기 설정 : 전체 200
+		jtMonthTotal.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtMonthTotal.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtMonthTotal.setRowHeight(25);
+		jtDayTotal.getColumnModel().getColumn(0).setPreferredWidth(200);
+		jtDayTotal.getColumnModel().getColumn(1).setPreferredWidth(140);
+		jtDayTotal.setRowHeight(25);
+
+		jspMT = new JScrollPane(jtMonthTotal);
+		jspDT = new JScrollPane(jtDayTotal);
+		jspMT.setBorder(new TitledBorder("총 매출 통계"));
+		jspDT.setBorder(new TitledBorder("총 매출 통계"));
+
 		setLayout(null);
-		
+
 		JLabel jlb1 = new JLabel("조회기간 설정");
 		jlb2 = new JLabel("~");
 		add(jlb1);
-		
+
 		add(jcbBeforeYear);
 		add(jcbBeforeMonth);
 		add(jcbBeforeDay);
@@ -64,8 +190,8 @@ public class PMStaticsView extends JPanel {
 		add(jcbAfterYear);
 		add(jcbAfterMonth);
 		add(jcbAfterDay);
-		
-		add(jcbStaticsChoice);
+
+		// add(jcbStaticsChoice);
 		add(jrbOperRate);
 		add(jrbFoodSell);
 		add(jrbTotalSell);
@@ -74,56 +200,98 @@ public class PMStaticsView extends JPanel {
 		add(jbtCreateGraph);
 		add(jlbResult);
 		
-		//1000, 600
-		jcbStaticsChoice.setBounds(30,20,140,25);
-		jlb1.setBounds(190,20,100,25);
-		jcbBeforeYear.setBounds(280,20,60,25);
-		jcbBeforeMonth.setBounds(340,20,40,25);
-		jcbBeforeDay.setBounds(380,20,40,25);
-		jlb2.setBounds(425,21,10,25);
-		jcbAfterYear.setBounds(440,20,60,25);
-		jcbAfterMonth.setBounds(500,20,40,25);
-		jcbAfterDay.setBounds(540,20,40,25);
-		jcbAfterYear.setVisible(false);
-		jcbAfterMonth.setVisible(false);
-		jcbAfterDay.setVisible(false);
-		jlb2.setVisible(false);
+		jlbResult.add(jspMT);
+		jlbResult.add(jspDT);
+		jlbResult.add(jspMF);
+		jlbResult.add(jspDF);
+		jlbResult.add(jspMO);
+		jlbResult.add(jspDO);
+		jlbResult.add(jspMax);
+		jlbResult.add(jspSum);
 		
-		jrbOperRate.setBounds(830,150,100,25);
-		jrbFoodSell.setBounds(830,180,100,25);
-		jrbTotalSell.setBounds(830,210,100,25);
-		jbtFoodDetail.setBounds(830,280,120,25);
-		jbtCreateTable.setBounds(830,350,120,40);
-		jbtCreateGraph.setBounds(830,400,120,40);
-		jlbResult.setBounds(30,60,750,490);
+		// 1000, 600
+//		jcbStaticsChoice.setBounds(30,20,140,25);
+//		jlb1.setBounds(190,20,100,25);
+//		jcbBeforeYear.setBounds(280,20,60,25);
+//		jcbBeforeMonth.setBounds(340,20,40,25);
+//		jcbBeforeDay.setBounds(380,20,40,25);
+//		jlb2.setBounds(425,21,10,25);
+//		jcbAfterYear.setBounds(440,20,60,25);
+//		jcbAfterMonth.setBounds(500,20,40,25);
+//		jcbAfterDay.setBounds(540,20,40,25);
+//		jcbAfterYear.setVisible(false);
+//		jcbAfterMonth.setVisible(false);
+//		jcbAfterDay.setVisible(false);
+//		jlb2.setVisible(false);
+//		jcbStaticsChoice.setBounds(30,20,140,25);
+		jlb1.setBounds(30, 20, 100, 25);
+		jcbBeforeYear.setBounds(120, 20, 60, 25);
+		jcbBeforeMonth.setBounds(180, 20, 40, 25);
+		jcbBeforeDay.setBounds(220, 20, 40, 25);
+		jlb2.setBounds(265, 21, 10, 25);
+		jcbAfterYear.setBounds(280, 20, 60, 25);
+		jcbAfterMonth.setBounds(340, 20, 40, 25);
+		jcbAfterDay.setBounds(380, 20, 40, 25);
+//		jcbAfterYear.setVisible(false);
+//		jcbAfterMonth.setVisible(false);
+//		jcbAfterDay.setVisible(false);
+//		jlb2.setVisible(false);
+
+		jrbOperRate.setBounds(830, 150, 100, 25);
+		jrbFoodSell.setBounds(830, 180, 100, 25);
+		jrbTotalSell.setBounds(830, 210, 100, 25);
+		jbtFoodDetail.setBounds(830, 280, 120, 25);
+		jbtCreateTable.setBounds(830, 350, 120, 40);
+		jbtCreateGraph.setBounds(830, 400, 120, 40);
+		jlbResult.setBounds(30, 60, 750, 490);
+		
+		jspMT.setBounds(20, 20, 340, 450);
+		jspDT.setBounds(20, 20, 340, 450);
+		jspMF.setBounds(20, 20, 340, 450);
+		jspDF.setBounds(20, 20, 340, 450);
+		jspMO.setBounds(20, 20, 340, 450);
+		jspDO.setBounds(20, 20, 340, 450);
+		jspMax.setBounds(390, 20, 340, 80);
+		jspSum.setBounds(390, 120, 340, 80);
+		
+		jspMT.setVisible(false);
+		jspDT.setVisible(false);
+		jspMF.setVisible(false);
+		jspDF.setVisible(false);
+		jspMO.setVisible(false);
+		jspDO.setVisible(false);
+		jspMax.setVisible(false);
+		jspSum.setVisible(false);
+//		jlbResult.setVisible(false);
+		
 
 		PMStaticsController pmsc = new PMStaticsController(this);
-		jcbStaticsChoice.addActionListener(pmsc);
+//		jcbStaticsChoice.addActionListener(pmsc);
 		jbtFoodDetail.addActionListener(pmsc);
 		jbtCreateTable.addActionListener(pmsc);
 		jbtCreateGraph.addActionListener(pmsc);
-		
+
 		jcbBeforeYear.addActionListener(pmsc);
 		jcbBeforeMonth.addActionListener(pmsc);
 		jcbAfterYear.addActionListener(pmsc);
 		jcbAfterMonth.addActionListener(pmsc);
 
 	}
-	
+
 	private void setDate(JComboBox<String> jcbYear, JComboBox<String> jcbMonth, JComboBox<String> jcbDay) {
 		DefaultComboBoxModel<String> dcbSetYear, dcbSetMonth, dcbSetDay;
 		Calendar cal = Calendar.getInstance();
-		
+
 		String year = "";
 		String month = "";
 		String day = "";
-		for(int i=cal.get(Calendar.YEAR); 2017<=i ; i--) {
+		for (int i = cal.get(Calendar.YEAR); 2017 <= i; i--) {
 			year += i + ",";
 		}
-		for(int i=1; i<=12 ; i++) {
+		for (int i = 1; i <= 12; i++) {
 			month += i + ",";
 		}
-		for(int i=1; i<=cal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
+		for (int i = 1; i <= cal.getActualMaximum(Calendar.DAY_OF_MONTH); i++) {
 			day += i + ",";
 		}
 		dcbSetYear = new DefaultComboBoxModel<String>(year.split(","));
@@ -132,15 +300,15 @@ public class PMStaticsView extends JPanel {
 		jcbYear.setModel(dcbSetYear);
 		jcbMonth.setModel(dcbSetMonth);
 		jcbDay.setModel(dcbSetDay);
-		
+
 		jcbYear.setSelectedIndex(0);
 		jcbMonth.setSelectedIndex(cal.get(Calendar.MONTH));
-		jcbDay.setSelectedIndex(cal.get(Calendar.DAY_OF_MONTH)-1);		
+		jcbDay.setSelectedIndex(cal.get(Calendar.DAY_OF_MONTH) - 1);
 	}
 
-	public JComboBox<String> getJcbStaticsChoice() {
-		return jcbStaticsChoice;
-	}
+//	public JComboBox<String> getJcbStaticsChoice() {
+//		return jcbStaticsChoice;
+//	}
 
 	public JRadioButton getJrbOperRate() {
 		return jrbOperRate;
@@ -166,11 +334,10 @@ public class PMStaticsView extends JPanel {
 		return jbtCreateGraph;
 	}
 
-	public JTextField getJlbResult() {
-		return jlbResult;
-	}
+//	public JTextField getJlbResult() {
+//		return jlbResult;
+//	}
 
-	
 	public JComboBox<String> getJcbBeforeYear() {
 		return jcbBeforeYear;
 	}
@@ -198,6 +365,69 @@ public class PMStaticsView extends JPanel {
 	public JLabel getJlb2() {
 		return jlb2;
 	}
-	
-	
+
+	public DefaultTableModel getDtmMonthOper() {
+		return dtmMonthOper;
+	}
+
+	public DefaultTableModel getDtmDayOper() {
+		return dtmDayOper;
+	}
+
+	public DefaultTableModel getDtmMonthFood() {
+		return dtmMonthFood;
+	}
+
+	public DefaultTableModel getDtmDayFood() {
+		return dtmDayFood;
+	}
+
+	public DefaultTableModel getDtmMonthTotal() {
+		return dtmMonthTotal;
+	}
+
+	public DefaultTableModel getDtmDayTotal() {
+		return dtmDayTotal;
+	}
+
+	public JScrollPane getJspMT() {
+		return jspMT;
+	}
+
+	public JScrollPane getJspDT() {
+		return jspDT;
+	}
+
+	public JScrollPane getJspMF() {
+		return jspMF;
+	}
+
+	public JScrollPane getJspDF() {
+		return jspDF;
+	}
+
+	public JScrollPane getJspMO() {
+		return jspMO;
+	}
+
+	public JScrollPane getJspDO() {
+		return jspDO;
+	}
+
+	public DefaultTableModel getDtmMaxData() {
+		return dtmMaxData;
+	}
+
+	public JScrollPane getJspMax() {
+		return jspMax;
+	}
+
+	public DefaultTableModel getDtmSumData() {
+		return dtmSumData;
+	}
+
+	public JScrollPane getJspSum() {
+		return jspSum;
+	}
+
 }
