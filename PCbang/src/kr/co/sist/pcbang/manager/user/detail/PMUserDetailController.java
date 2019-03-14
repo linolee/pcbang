fe069aae02460534e4bf5250e45aa1edf7d82acc
InJaEE <in11202@naver.com>
2019-03-14 18:12:01 +0900

    ㅎㅎ

--- PCbang/src/kr/co/sist/pcbang/manager/user/detail/PMUserDetailController.java
+++ PCbang/src/kr/co/sist/pcbang/manager/user/detail/PMUserDetailController.java
@@ -32,22 +32,43 @@ public class PMUserDetailController extends WindowAdapter implements ActionListe
 				(udv.getJtfEmail().getText(), udv.getJtfTel().getText(), udv.getJtfBirth().getText(), udv.getJtfId().getText(), 
 						Integer.parseInt(udv.getJtfLeftTime().getText()));
 		
+			if(!udv.getJtfEmail().getText().contains("@")) {
+				JOptionPane.showMessageDialog(udv, "잘못된 이메일 형식입니다");
+				return;
+			}
+			if(udv.getJtfTel().getText().length()<=10 ) {
+				JOptionPane.showMessageDialog(udv, "잘못된 전화번호 형식입니다");
+				return;
+			}
+			if(!(udv.getJtfBirth().getText().length()==8) ) {
+				JOptionPane.showMessageDialog(udv, "잘못된 생년월일 형식입니다");
+				return;
+			}
+			if(Integer.parseInt(udv.getJtfLeftTime().getText())>=10000) {
+				JOptionPane.showMessageDialog(udv, "잔여시간은 0~9999입니다");
+				return;
+				
+			}
+		
 			if(u_dao.updateMemberData(upvo)) {
 				JOptionPane.showMessageDialog(udv, "회원정보가 변경되었습니다");
 				udv.dispose();
 				uc.selectUser();
 			}
 		}catch (SQLException e) {
-			JOptionPane.showMessageDialog(udv, "스미마셍");
-			e.printStackTrace();
+			JOptionPane.showMessageDialog(udv, "입력값을 확인해주세요");
 		} catch(NullPointerException ne) {
 			JOptionPane.showMessageDialog(udv, "빈칸을 입력할 수 없습니다");
-		} catch(NumberFormatException nfe) {
-		JOptionPane.showMessageDialog(udv, "빈칸을 입력할 수 없습니다");
+		} 
+		catch(NumberFormatException nfe) {
+		JOptionPane.showMessageDialog(udv, "입력값을 확인해주세요");
 	} 
 		
 	}
 	
+
+
+
 	@Override
 	public void windowClosing(WindowEvent e) {
 		udv.dispose();
